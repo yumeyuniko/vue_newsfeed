8745aa1227dd3bd22f4a0c0a5736a6ab2631f551
yumeyuniko <daigocafe@msn.com>
2022-03-20 16:52:53 +0900

    SidebarNav

--- src/components/SidebarNav.vue
+++ src/components/SidebarNav.vue
@@ -4,15 +4,30 @@
       menu_is_active ? 'translate-x-0' : '-translate-x-full'
     }`"
   >
-    <h2 class="text-gray-500 text-sm font-bold uppercase mb-2">Menu</h2>
+    <div class="p-4">
+      <h2 class="text-gray-500 text-sm font-bold uppercase mb-2">Menu</h2>
+      <div class="-mx-4">
+        <MenuItem to="/">
+          <span class="material-icons mr-2">home</span> Home
+        </MenuItem>
+        <MenuItem to="/authors">
+          <span class="material-icons mr-2">groups</span> Authors
+        </MenuItem>
+      </div>
+    </div>
   </aside>
 </template>
 
 <script>
 import { computed } from 'vue';
 import { useStore } from 'vuex';
+import MenuItem from './MenuItem.vue';
 
 export default {
+  components: {
+    MenuItem,
+  },
+
   setup() {
     const store = useStore();
 
