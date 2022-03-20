bbaabf9313dab3b66376f0e8a4e440ea2081283a
yumeyuniko <daigocafe@msn.com>
2022-03-20 15:34:57 +0900

    hamburgerMenu

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <nav class="bg-green-500">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="bg-gray-700 min-h-screen text-white">
+    <HeaderNav />
+    <router-view />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HeaderNav from './components/HeaderNav.vue';
 
-nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    HeaderNav,
+  },
+};
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
