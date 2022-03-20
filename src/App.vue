8376faddada3ae8895db4ff0934b25a52283e248
yumeyuniko <daigocafe@msn.com>
2022-03-20 15:52:50 +0900

    SidebarNav

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="bg-gray-700 min-h-screen text-white">
     <HeaderNav />
+    <SidebarNav />
     <router-view />
   </div>
 </template>
 
 <script>
 import HeaderNav from './components/HeaderNav.vue';
+import SidebarNav from './components/SidebarNav.vue';
 
 export default {
   components: {
     HeaderNav,
+    SidebarNav,
   },
 };
 </script>
---
+++ src/components/SidebarNav.vue
@@ -0,0 +1,24 @@
+<template>
+  <aside
+    :class="`fixed top-0 left-0 min-h-screen pt-16 z-40 w-full max-w-xs transform duration-300 bg-gray-800 ${
+      menu_is_active ? 'translate-x-0' : '-translate-x-full'
+    }`"
+  >
+    <h2 class="text-gray-500 text-sm font-bold uppercase mb-2">Menu</h2>
+  </aside>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+  setup() {
+    const store = useStore();
+
+    return {
+      menu_is_active: computed(() => store.state.menu_is_active),
+    };
+  },
+};
+</script>
